6440a3a74a9dbe84c3ab5e82a6026d921182110f
ANU-2524 <anusoni25.2006@gmail.com>
2025-10-05 00:33:22 +0530

    fix: update CORS for renamed site

--- core/api.py
+++ core/api.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # Configure CORS middleware for frontend origin (adjust port if needed)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173" , "https://nextgenlingo-genai.netlify.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
